2fdf6514a341fd53bbec5252c88ea49afee4f7a2
Philipp Schlegel <observing@web.de>
2024-02-25 15:18:31 +0000

    various improvements: - drop unnecessary copies / initialiations - use SIMD for distance calculations - update tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
+#![feature(portable_simd)]
+
 use ndarray::{Array1, Array2, ArrayView1, ArrayView2, Axis, s};
 use numpy::{IntoPyArray, PyArray1, PyReadonlyArray2, PyReadonlyArray1};
 use pyo3::prelude::*;
-use pyo3::prelude::Python;
+use core::simd::prelude::f64x4;
+use std::simd::num::SimdFloat;
 
-/// A Delaunay triangulation.
+
+
+/// A neighborhood graph.
 ///
 /// Arguments:
 ///  points: (N, 3) Array of points in the triangulation
@@ -12,16 +17,28 @@ use pyo3::prelude::Python;
 ///
 /// To get the neighbors of point `i``, we need need use:
 /// neighbors[indices[i]:indices[i+1]]
-struct Delaunay {
+struct Neighboorhood {
     // The (N, 3) points in the triangulation
     points: Array2<f64>,
     // The indices of neighboring vertices of vertex k are
     // neighbors[indices[k]:indices[k+1]].
     indices: Array1<usize>,
     neighbors: Array1<usize>,
+    points_simd: Vec<f64x4>,
 }
 
-/// Find all nearest neighbors between two point clouds using Delaunay triangulations.
+impl Neighboorhood {
+    fn new(points: Array2<f64>, indices: Array1<usize>, neighbors: Array1<usize>) -> Neighboorhood {
+        let mut points_simd: Vec<f64x4> = vec![];
+        for p in points.outer_iter() {
+            points_simd.push(f64x4::from_array([p[0], p[1], p[2], 0.0]));
+        }
+
+        Neighboorhood {points, indices, neighbors, points_simd}
+    }
+}
+
+/// Find all nearest neighbors between two point clouds using neighboorhood graphs.
 ///
 /// Arguments:
 ///  x_points: (N, 3) Array of points in the first point cloud
@@ -41,16 +58,8 @@ fn all_nearest_neighbours<'py>(
     y_neighbors: PyReadonlyArray1<usize>,
     ) -> PyResult<(Py<PyArray1<f64>>, Py<PyArray1<usize>>)> {
     // Convert inputs to Delaunay structs
-    let x = Delaunay {
-        points: x_points.to_owned_array(),
-        indices: x_indices.to_owned_array(),
-        neighbors: x_neighbors.to_owned_array(),
-    };
-    let y = Delaunay {
-        points: y_points.to_owned_array(),
-        indices: y_indices.to_owned_array(),
-        neighbors: y_neighbors.to_owned_array(),
-    };
+    let x = Neighboorhood::new(x_points.to_owned_array(), x_indices.to_owned_array(), x_neighbors.to_owned_array());
+    let y = Neighboorhood::new(y_points.to_owned_array(), y_indices.to_owned_array(), y_neighbors.to_owned_array());
 
     // Prepare outputs
     let mut distances = Array1::zeros(x_indices.len() - 1);
@@ -62,19 +71,23 @@ fn all_nearest_neighbours<'py>(
     // Find the nearest neighbor of the first vertex in `x` in `y`
     let mut d: f64;
     let mut ix: usize;
-    (d, ix) = _find_nearest_neighbour(&y, &x.points.row(vertex), 0);
+    let mut visited_y = vec![false; y.points.nrows()];
+    (d, ix) = find_nearest_neighbour_simd(&y, &x.points_simd[vertex], 0, &mut visited_y);
     distances[vertex] = d;
     indices[vertex] = ix;
 
     // Our stack will contain a vertex in `y` and a vertex in `x` that
     // we expect to be in the vicinity of `y`'s nearest neighbor
-    let mut stack: Vec<(usize, usize)> = vec![];
-    let mut visited = Array1::from_elem(x.indices.len(), false);
+    // Note: we know our stack will never be bigger than the number of
+    // vertices in `x`. This avoids reallocations.
+    let mut stack: Vec<(usize, usize)> = Vec::with_capacity(x.indices.len());
+    let mut visited_x = Array1::from_elem(x.indices.len(), false);
     for n in get_neighbours(&x, vertex) {
+        let n = n.clone();
         stack.push((ix, n));
-        visited[n] = true;
+        visited_x[n] = true;
     }
-    // let mut stack = vec![(ix, get_neighbours(&x, vertex))];
+
     let mut current_ix: usize;
     let mut n: usize;
     while stack.len() > 0 {
@@ -84,13 +97,15 @@ fn all_nearest_neighbours<'py>(
         let new_ix = current_ix;
 
         // Find the nearest neighbor of the current vertex in `y`
-        (d, ix) = _find_nearest_neighbour(&y, &x.points.row(n), new_ix);
+        //(d, ix) = find_nearest_neighbour(&y, &x.points.row(n), new_ix, &mut visited_y);
+        (d, ix) = find_nearest_neighbour_simd(&y, &x.points_simd[n], new_ix, &mut visited_y);
         distances[n] = d;
         indices[n] = ix;
 
-        for n in get_neighbours(&x, n) {
-            if !visited[n] {
-                visited[n] = true;
+        for n2 in get_neighbours(&x, n) {
+            n = n2.clone();
+            if !visited_x[n] {
+                visited_x[n] = true;
                 stack.push((ix, n));
             }
         }
@@ -99,18 +114,34 @@ fn all_nearest_neighbours<'py>(
     }
 
     // Turn the vectors into numpy arrays
-    let distances_py: Py<PyArray1<f64>> = distances.into_pyarray(py).to_owned();
-    let indices_py: Py<PyArray1<usize>> = indices.into_pyarray(py).to_owned();
+    Ok((distances.into_pyarray(py).to_owned(), indices.into_pyarray(py).to_owned()))
 
-    Ok((distances_py, indices_py))
+}
 
+macro_rules! assert_equal_len {
+    ($a:ident, $b: ident) => {
+        assert!($a.len() == $b.len(),
+                "add_assign: dimension mismatch: {:?} += {:?}",
+                ($a.len(),),
+                ($b.len(),));
+    }
 }
 
 /// Calculate the squared euclidean distance between two points.
 fn euclidean_distance(a: &ArrayView1<f64>, b: &ArrayView1<f64>) -> f64 {
+    assert_equal_len!(a, b);
     //(a - b).mapv(|x| x.powi(2)).sum()
     // For some reason this is faster than the above:
-    (a[0] - b[0]).powi(2) + (a[1] - b[1]).powi(2) + (a[2] - b[2]).powi(2)
+    let mut d: f64 = 0.0;
+    for (aa, bb) in a.iter().zip(b.iter()) {
+        d += (aa - bb).powi(2);
+    }
+    d
+}
+
+fn euclidean_distance_simd(a: &f64x4, b: &f64x4) -> f64 {
+    let diff = a - b;
+    (diff * diff).reduce_sum()
 }
 
 /// Calculate the squared euclidean distance between a point and a set of points.
@@ -126,24 +157,101 @@ fn euclidean_distance_to_point(p: &ArrayView1<f64>, points: &ArrayView2<f64>) ->
 /// Find the approximate nearest neighbor of point p among the points in y.
 ///
 /// Arguments:
-///  y: Delaunay triangulation of the points to search in
+///  y: Neighboorhood graph of the points to search in
 ///  p: (N, 3) Point to find the nearest neighbor in `y` for
 ///  start: Index of a point in y where to start the search from
+///  visited: Array of bools to keep track of which points we have already visited
 ///
 /// Returns:
 ///  d: The distance between p and the nearest neighbor
 ///  vertex: The index of the nearest neighbor
-fn _find_nearest_neighbour(y: &Delaunay, p: &ArrayView1<f64>, start: usize) -> (f64, usize) {
+fn find_nearest_neighbour(y: &Neighboorhood, p: &ArrayView1<f64>, start: usize, visited: &mut Array1<bool>) -> (f64, usize) {
     let mut vertex: usize = start;
 
-    // Track which points we have already visited
-    let mut visited = Array1::from_elem(y.indices.len(), false);
+    // Track which points we have already been visited
+    visited.fill(false); // reset the visited array
     visited[vertex] = true;
 
     // Get the distance between p and the starting point
     let mut d = euclidean_distance(&y.points.row(vertex), &p);
 
-    let mut neighbors: Array1<usize>;
+    let mut neighbors: ArrayView1<usize>;
+    let mut vert_new: bool;
+    let mut d_new: f64;
+    let mut p2: ArrayView1<f64>;
+    loop {
+        // Get the neighbours of the current vertex
+        neighbors = get_neighbours(&y, vertex);
+        // let coordinates = y.points.select(Axis(0), &neighbors.to_vec());
+
+        // Figure out if any of the neighbors is closer to p than the current vertex
+        // If so, update the distance and the current vertex
+        vert_new = false;
+        for n in neighbors {
+            let n = n.clone();  // This is necessary because `neighbors` is a unowned slice
+            assert!(n < visited.len());
+            if !visited[n] {
+                // No matter what happens we can mark this vertex as visited
+                visited[n] = true;
+
+                // d_new = euclidean_distance(&y.points.row(n), &p);
+
+                // Incrementally build the distance - that way we can stop early
+                // if we know that the current vertex is farther away than the
+                // closest vertex we have found so far
+                p2 = y.points.row(n);
+                d_new = (p[0] - p2[0]).powi(2);
+                if d_new > d {
+                    continue;
+                }
+                d_new += (p[1] - p2[1]).powi(2);
+                if d_new > d {
+                    continue;
+                }
+                d_new += (p[2] - p2[2]).powi(2);
+                if d_new > d {
+                    continue;
+                }
+
+                // If we got to here, we know that the current vertex is closer
+                // than the closest vertex we have found so far
+                d = d_new;
+                vert_new = true;
+                vertex = n;
+            }
+        }
+        // If none of the neighbours is closer than the current vertex, we are done
+        if !vert_new {
+            break;
+        }
+        }
+    (d.sqrt(), vertex)
+    }
+
+/// Find the approximate nearest neighbor of point p among the points in y.
+///
+/// This version uses SIMD to calculate the distances between the points.
+///
+/// Arguments:
+///  y: Neighboorhood graph of the points to search in
+///  p: (N, 3) Point to find the nearest neighbor in `y` for
+///  start: Index of a point in y where to start the search from
+///  visited: Array of bools to keep track of which points we have already visited
+///
+/// Returns:
+///  d: The distance between p and the nearest neighbor
+///  vertex: The index of the nearest neighbor
+fn find_nearest_neighbour_simd(y: &Neighboorhood, p: &f64x4, start: usize, visited: &mut Vec<bool>) -> (f64, usize) {
+    let mut vertex: usize = start;
+
+    // Track which points have already been visited
+    visited.fill(false); // reset the visited array
+    visited[vertex] = true;
+
+    // Get the distance between p and the starting point
+    let mut d = euclidean_distance_simd(&y.points_simd[vertex], &p);
+
+    let mut neighbors: ArrayView1<usize>;
     let mut vert_new: bool;
     let mut d_new: f64;
     loop {
@@ -155,11 +263,15 @@ fn _find_nearest_neighbour(y: &Delaunay, p: &ArrayView1<f64>, start: usize) -> (
         // If so, update the distance and the current vertex
         vert_new = false;
         for n in neighbors {
+            let n = n.clone();  // This is necessary because `neighbors` is a unowned slice
+            assert!(n < visited.len());
             if !visited[n] {
-                d_new = euclidean_distance(&y.points.row(n), &p);
                 // No matter what happens we can mark this vertex as visited
                 visited[n] = true;
-                // Check if this vertex is closer than the current vertex
+
+                // Calculate the distance
+                d_new = euclidean_distance_simd(&y.points_simd[n], &p);
+                // Check if the new distance is smaller than the current distance
                 if d_new < d {
                     d = d_new;
                     vert_new = true;
@@ -183,13 +295,12 @@ fn _find_nearest_neighbour(y: &Delaunay, p: &ArrayView1<f64>, start: usize) -> (
 ///
 /// Returns:
 ///  Array of indices of the neighbours of `vertex`
+#[inline(always)]
 fn get_neighbours(
-    x: &Delaunay,
+    x: &Neighboorhood,
     vertex: usize
-    ) -> Array1<usize> {
-    let ix1 = x.indices[vertex];
-    let ix2 = x.indices[vertex + 1];
-    x.neighbors.slice(s![ix1..ix2]).to_owned()
+    ) -> ArrayView1<usize> {
+    x.neighbors.slice(s![x.indices[vertex]..x.indices[vertex + 1]])
     }
 
 /// A Python module implemented in Rust.
--- tests/test_aann.py
+++ tests/test_aann.py
@@ -4,12 +4,11 @@ import time
 import numpy as np
 import pandas as pd
 
-from scipy.spatial import Delaunay
-import time
+from scipy.spatial import Delaunay, KDTree
 
 
 def test_random():
-    N = 10000
+    N = 100000
     x = Delaunay(np.random.rand(N, 3))
     y = Delaunay(np.random.rand(N, 3))
 
@@ -43,6 +42,8 @@ def test_neurons():
         sep=" ",
     )[[2, 3, 4]].values
 
+    print(f"Testing neurons with {x.shape[0]:,} and {y.shape[0]:,} points")
+
     N = x.shape[0]
 
     start_time = time.time()
@@ -50,6 +51,9 @@ def test_neurons():
     y = Delaunay(y)
     end_time = time.time()
     print(f"Generating Delaunay: {end_time - start_time:.2f} seconds")
+
+    # Run one warm-up
+    _ = aann.all_nearest_neighbours(x, y)
 
     start_time = time.time()
     d, i = aann.all_nearest_neighbours(x, y)
@@ -64,3 +68,94 @@ def test_neurons():
 
     assert np.all(i >= 0)
     assert np.all(i < N)
+
+
+def test_neurons_kdtree():
+    # Load two example neurons as point clouds
+    x = pd.read_csv(
+        "/Users/philipps/Downloads/720575940638426064.swc",
+        comment="#",
+        header=None,
+        sep=" ",
+    )[[2, 3, 4]].values
+    y = pd.read_csv(
+        "/Users/philipps/Downloads/720575940613656978.swc",
+        comment="#",
+        header=None,
+        sep=" ",
+    )[[2, 3, 4]].values
+
+    N = x.shape[0]
+
+    start_time = time.time()
+    tree = KDTree(y)
+    end_time = time.time()
+    print(f"Generating KDTree: {end_time - start_time:.2f} seconds")
+
+    # Run one warm-up
+    _ = tree.query(x)
+
+    start_time = time.time()
+    d, i = tree.query(x)
+    end_time = time.time()
+
+    print("Distance to nearest neighbor for each point in x:", d)
+    print("Index of nearest neighbor for each point in x:", i)
+    print(f"Tree query time: {end_time - start_time:.2f} seconds")
+
+
+def _neuron_to_fake_delaunay(swc):
+    points = swc[[2, 3, 4]].values.astype(np.float64, copy=False)
+    parents = swc[6].values
+    nodes = swc[0].values
+    indices = []
+    neighbors = []
+    for i, n in enumerate(nodes):
+        # Start
+        indices.append(len(neighbors))
+        # Add this node's parent as neighbor
+        if parents[i] >= 0:
+            neighbors.append(parents[i])
+        # Add all children as neighbors
+        neighbors += list(np.where(parents == n)[0])
+        # End
+        indices.append(len(neighbors))
+    return np.array(indices, dtype=np.uint64), np.array(neighbors, dtype=np.uint64), points
+
+"""
+def test_neurons_no_delaunay():
+    # Load two example neurons as point clouds
+    swc1 = pd.read_csv(
+        "/Users/philipps/Downloads/720575940638426064.swc",
+        comment="#",
+        header=None,
+        sep=" ",
+    )
+    ind1, neigh1, points1 = _neuron_to_fake_delaunay(swc1)
+    swc2 = pd.read_csv(
+        "/Users/philipps/Downloads/720575940613656978.swc",
+        comment="#",
+        header=None,
+        sep=" ",
+    )
+    ind2, neigh2, points2 = _neuron_to_fake_delaunay(swc2)
+
+    start_time = time.time()
+    d, i = aann._aann.all_nearest_neighbours(
+        points1,
+        ind1,
+        neigh1,
+        points2,
+        ind2,
+        neigh2
+    )
+    end_time = time.time()
+
+    print("Distance to nearest neighbor for each point in x:", d)
+    print("Index of nearest neighbor for each point in x:", i)
+    print(f"Execution time: {end_time - start_time:.2f} seconds")
+"""
+
+if __name__ == "__main__":
+    test_neurons()
+    test_neurons_kdtree()
